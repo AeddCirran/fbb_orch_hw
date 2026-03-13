9417dd5360b4357fee91879f4b0c6b77c3c6f641
Vladislav Labanov <128789419+AeddCirran@users.noreply.github.com>
2026-03-13 22:42:56 +0300

    Heal task1

--- hw1/complement.py
+++ hw1/complement.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,  # или DEBUG
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M",
+)
 
 
 def rev_compl(seq: str) -> str:
@@ -9,7 +18,7 @@ def rev_compl(seq: str) -> str:
 
 
 def main():
-    raise RuntimeError("Fail task1")
+    logging.info("Starting script")
     parser = argparse.ArgumentParser(
         description="Print rev-compl and GC-content of DNA/RNA seq"
     )
@@ -21,7 +30,9 @@ def main():
     args = parser.parse_args()
 
     rc_seq = rev_compl(args.seq)
+    logging.info("Rev-compl seq: %s", rc_seq)
     gc_cont = (rc_seq.upper().count("G") + rc_seq.upper().count("C")) / len(rc_seq)
+    logging.info("GC content: %.3f", gc_cont)
     print(f'{rc_seq}\n{gc_cont:.3f}')
 
 
