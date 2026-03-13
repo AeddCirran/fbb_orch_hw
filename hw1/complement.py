7ca6ed45ef3a9d96d569cf0a1b0251c9eeda5015
Vladislav Labanov <labanovvlad3125@gmail.com>
2026-03-13 14:41:01 +0300

    task1

---
+++ hw1/complement.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+
+
+def rev_compl(seq: str) -> str:
+    compl = seq.maketrans("ACGTUacgtu", "ACGTUacgtu")
+    return seq.translate(compl)[::-1]
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Print rev-compl and GC-content of DNA/RNA seq")
+    parser.add_argument(
+        "--seq",
+        required=True,
+        help="DNA/RNA seq",
+    )
+    args = parser.parse_args()
+    rc_seq = rev_compl(args.seq)
+    gc_cont = (rc_seq.upper().count("G") + rc_seq.upper().count("C")) / len(rc_seq)
+    print(f'{rc_seq}\n{gc_cont:.3f}')
+
+
+if __name__ == "__main__":
+    main()
